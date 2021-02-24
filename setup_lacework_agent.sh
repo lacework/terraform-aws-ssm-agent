a2d5bdba8d7924de57ac705ae0f4be02c2a7df99
Jean-Philippe Lachance <jplachance@coveo.com>
2021-02-24 16:13:07 -0500

    fix(command): Fix tags in the config.json file setup refactor(command): Always override the config.json file
    Signed-of-by: Jean-Philippe Lachance <jplachance@coveo.com>

--- setup_lacework_agent.sh
+++ setup_lacework_agent.sh
@@ -5,9 +5,8 @@ set -e
 LACEWORK_INSTALL_PATH="{{ LaceworkInstallPath }}"
 
 # TODO: Fetch the token from AWS SSM Parameter Store instead of taking it in as a Command parameter (avoid leaks in the AWS Console)
-TOKEN="{{ Token }}"
-TAGS="{{ Tags }}"
-
+TOKEN='{{ Token }}'
+TAGS='{{ Tags }}'
 
 # TODO: Handle systems that don't have systemctl
 if systemctl is-active --quiet kubelet; then
@@ -19,7 +18,7 @@ if [ ! -d "$LACEWORK_INSTALL_PATH" ]; then
   echo "Lacework agent not installed, installing..."
 
   # TODO: Add the support for hosts that don't have curl installed
-  curl https://packages.lacework.net/install.sh > /tmp/install.sh
+  curl https://packages.lacework.net/install.sh >/tmp/install.sh
 
   chmod +x /tmp/install.sh
 
@@ -27,11 +26,11 @@ if [ ! -d "$LACEWORK_INSTALL_PATH" ]; then
   sudo /tmp/install.sh "$TOKEN"
 
   rm /tmp/install.sh
-else
-  echo "Lacework is already installed, configuring the agent token..."
+fi
 
-  # TODO: Add the support for other Lacework configuration options
-  cat > "$LACEWORK_INSTALL_PATH/config/config.json" <<EOF
+# TODO: Add the support for other Lacework configuration options
+echo "Updating the Lacework agent config.json file..."
+cat >"$LACEWORK_INSTALL_PATH/config/config.json" <<EOF
 {
   "tokens": {
     "AccessToken": "$TOKEN"
@@ -39,6 +38,5 @@ else
   "tags": $TAGS
 }
 EOF
-fi
 
 echo "Lacework configured successfully!"
