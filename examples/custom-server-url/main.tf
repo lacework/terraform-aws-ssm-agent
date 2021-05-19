3f5731f644312dacb2fe03d60b5992f0accdd7e2
Salim Afiune Maya <afiune@lacework.net>
2021-05-19 14:53:50 -0700

    docs: add custom server url example
    Signed-off-by: Salim Afiune Maya <afiune@lacework.net>

---
+++ examples/custom-server-url/main.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+provider "lacework" {}
+
+resource "lacework_agent_access_token" "ssm_deployment" {
+  name        = "ssm-deployment"
+  description = "Used to deploy agents using AWS System Manager"
+}
+
+module "lacework_ssm_agents_install_custom_server_url" {
+  source = "../../"
+
+  lacework_access_token = lacework_agent_access_token.ssm_deployment.token
+  lacework_server_url   = "https://api.fra.lacework.net"
+}
+
+resource "aws_resourcegroups_group" "testing" {
+  name = "Testing"
+
+  resource_query {
+    query = jsonencode({
+      ResourceTypeFilters = [
+        "AWS::EC2::Instance"
+      ]
+    })
+  }
+}
+
+resource "aws_ssm_association" "lacework_aws_ssm_agents_install_testing" {
+  association_name = "install-lacework-agents-testing-group"
+
+  name = module.lacework_aws_ssm_agents_install.ssm_document_name
+
+  targets {
+    key = "resource-groups:Name"
+    values = [
+      aws_resourcegroups_group.testing.name,
+    ]
+  }
+}
