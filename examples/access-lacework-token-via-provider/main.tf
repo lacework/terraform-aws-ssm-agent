ad846e354a7c5025efd252f856f81068f90fea81
Darren <75614232+dmurray-lacework@users.noreply.github.com>
2021-05-19 17:09:23 +0100

    docs: Use EU url in example

--- examples/access-lacework-token-via-provider/main.tf
+++ examples/access-lacework-token-via-provider/main.tf
@@ -22,7 +22,7 @@ module "lacework_aws_ssm_agents_install" {
   }
 
   lacework_access_token = lacework_agent_access_token.ssm_deployment.token
-  lacework_server_url = "https://api.lacework.net"
+  lacework_server_url = "https://api.fra.lacework.net"
 }
 
 resource "aws_resourcegroups_group" "testing" {
