ee5bb9b397a69b59a52ad1428b7e5c7cadf7d107
Darren Murray <darren.murray@lacework.net>
2024-01-23 16:51:44 +0000

    chore: set local var module name
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 locals {
   version_file   = "${abspath(path.module)}/VERSION"
-  module_name    = basename(abspath(path.module))
+  module_name    = "terraform-aws-ssm-agent"
   module_version = fileexists(local.version_file) ? file(local.version_file) : ""
 }
 
